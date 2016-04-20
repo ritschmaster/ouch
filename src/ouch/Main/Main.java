a9a76ae1671621b8bf7674c752a9264801eca6bd
alexander <alexander@youknowmy.mail>
2016-04-20 19:00:39 +0200

    Test

--- src/ouch/Main/Main.java
+++ src/ouch/Main/Main.java
@@ -30,6 +30,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("I am testing my shit");
 
     }
 
