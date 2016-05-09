96c434e84c51d637e8708d6eb75bd6ecbcf49c52
alexander <alexander@youknowmy.mail>
2016-05-09 22:40:16 +0200

    added accidentally removed license header

--- src/ouch/transcoders/Compressions/LZ77Transcoder.java
+++ src/ouch/transcoders/Compressions/LZ77Transcoder.java
@@ -1,4 +1,27 @@
-	package ouch.transcoders.Compressions;
+
+/************************************************************************
+ * Copyright (C) 2016 Richard Paul Bäck <richard.baeck@free-your-pc.com>
+ * 					  Dominik Koller <kollerdominik@icloud.com>
+ * 					  Alexander Kopp <alexander.kopp@gmx.at>
+ * 					  Zhe Wu <wuzhe1996@gmail.com>
+ *
+ * This file is part of OUCH.
+ *
+ * OUCH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OUCH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OUCH. If not, see <http://www.gnu.org/licenses/>.
+ ***********************************************************************/
+
+package ouch.transcoders.Compressions;
 
 import java.sql.Timestamp;
 import java.util.LinkedList;
